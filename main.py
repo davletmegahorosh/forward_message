a0d9dd762a32cb989633b0287f650902c4fc5fc1
davletmegahorosh <azimovdavlet460@gmail.com>
2023-10-17 19:23:37 +0600

    fixed bugs

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ async def send_message(text, time, username, client):
                 send_text = (f'{text}\n'
                         f'{time}\n'
                         f'<a href="https://t.me/{username}">Перейти в личку</a>\n')
-                await client.send_message(-1001989879370, send_text)  # -1001819329546
-                print('nigaday')
+                await client.send_message(-1001989879370, send_text)
 
+# -1001819329546
 def check_db(send_text):
     for text in quries.get_text():
         if list(text)[1] == send_text:
@@ -23,9 +23,10 @@ def check_db(send_text):
 @client.on_message()
 async def forward_messages_to_channel(client, message: types.Message):
     quries.drop_db()
+
     current_time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace('-', '.')
-    if message.chat.id != -1001989879370 and message.from_user.username\
-        and check_db(message.text) :#-1001819329546
+    if message.from_user and message.chat.id != -1001989879370 and message.from_user.username\
+        and check_db(message.text) :
         if message.photo and message.caption:
             await send_message(message.caption,current_time_str, message.from_user.username, client)
             quries.save_text(message.text)
@@ -37,6 +38,7 @@ async def forward_messages_to_channel(client, message: types.Message):
 
 if __name__ == "__main__":
     quries.init_db()
+    quries.create_tables()
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     client.run()
